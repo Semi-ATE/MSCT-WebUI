1362d00b01ee95f0f706154fe778f2f626d80400
dsk <github@awinia.de>
2022-10-25 15:56:49 +0200

    Use string interpolation

--- projects/msct-webui/src/app/constants.ts
+++ projects/msct-webui/src/app/constants.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL_RUNNING_IN_PYTHON_MASTER_APPLICATION = 'ws://${location.hostname}:80/ws';
+export const BACKEND_URL_RUNNING_IN_PYTHON_MASTER_APPLICATION = `ws://${location.hostname}:80/ws`;
 export const BACKEND_MOCKED = false;
